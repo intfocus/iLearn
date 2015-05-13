7cd7ab4910f98ec956d16ec542a37a1494bc5b3c
Charlie Hung <timestop.tw@gmail.com>
2015-05-13 22:37:25 +0800

    Initial Commit

---
+++ iLearn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iLearn
+//
+//  Created by Charlie Hung on 2015/5/13.
+//  Copyright (c) 2015 intFocus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
