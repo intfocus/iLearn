b2a045b505af9835090b451992ef0207bd38928b
Charlie Hung (RD-TW) <charlie_hung@trend.com.tw>
2015-06-20 02:17:42 +0800

    Add source files from iSearch

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -96,6 +96,12 @@
 #define COCOAPODS_VERSION_MINOR_FXBlurView 6
 #define COCOAPODS_VERSION_PATCH_FXBlurView 3
 
+// JTCalendar
+#define COCOAPODS_POD_AVAILABLE_JTCalendar
+#define COCOAPODS_VERSION_MAJOR_JTCalendar 1
+#define COCOAPODS_VERSION_MINOR_JTCalendar 2
+#define COCOAPODS_VERSION_PATCH_JTCalendar 3
+
 // MBProgressHUD
 #define COCOAPODS_POD_AVAILABLE_MBProgressHUD
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
@@ -107,4 +113,10 @@
 #define COCOAPODS_VERSION_MAJOR_QRCodeReaderViewController 3
 #define COCOAPODS_VERSION_MINOR_QRCodeReaderViewController 4
 #define COCOAPODS_VERSION_PATCH_QRCodeReaderViewController 0
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 2
+#define COCOAPODS_VERSION_PATCH_Reachability 0
 
