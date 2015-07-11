b82de4c164b7e93d1a809a0c411f2a2f2c47d77a
jay16 <jay_li@intfocus.com>
2015-07-11 18:56:50 +0800

    考试列表按结束时间降序，按考试名称升序.

--- iLearn/Source/Login/LoginViewController.m
+++ iLearn/Source/Login/LoginViewController.m
@@ -139,7 +139,7 @@
 }
 
 - (IBAction)actionSubmit:(id)sender {
-//    self.cookieValue = @"E00516";
+//    self.cookieValue = @"E99658601";
 //    [self performSelector:@selector(actionOutsideLoginSuccessfully:) withObject:self];
 //    return;
     
