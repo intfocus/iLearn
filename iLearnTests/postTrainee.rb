0e1267f006d0305970577956f3f965bfc7c99e11
jay16 <jay_li@intfocus.com>
2015-08-18 10:39:59 +0800

    课程ID未设置 popupView.

--- iLearnTests/postTrainee.rb
+++ iLearnTests/postTrainee.rb
@@ -1,16 +1,33 @@
 require "rest-client"
 require "json"
-#url = "http://tsa-china.takeda.com.cn/uat/api/Trainee_Api.php"
-#
-#hash = { UserId: "1",
-#    TrainingId: "3"
-#}
-#puts hash
-#
-#response = RestClient.post url, hash.to_json
-#puts response.headers
-#
 
+
+def postForm(url, param, info)
+    begin
+        response = RestClient.post url, param.to_json
+        
+        if response.code == 200
+            puts "#{info} 成功."
+        else
+            puts "#{info} 失败."
+            puts response.headers
+        end
+    rescue => e
+      puts "#{info} 失败 for #{e.message}"
+    end
+end
+
+# 学员报名信息维护
+url = "http://tsa-china.takeda.com.cn/uat/api/Trainee_Api.php"
+
+hash = {
+    UserId: "1",
+    TrainingId: "3"
+}
+postForm(url, hash, "学员报名信息维护")
+
+
+# 上传点名信息维护
 url = "http://tsa-china.takeda.com.cn/uat/api/RollCall_Api.php"
 hash = {
     TrainingId: "1",
@@ -20,5 +37,16 @@ hash = {
     Reason: "等快递快递收到伐",
     CreatedUser: "1"
 }
-response = RestClient.post url, hash.to_json
-puts response.headers
+postForm(url, hash, "上传点名信息维护")
+
+
+# 创建签到
+url = "http://tsa-china.takeda.com.cn/uat/api/CheckIn_Api.php"
+hash = {
+    UserId: "8",#创建用户
+    CheckInName: "ccssdd",#签到名称
+    CheckInId: "5",#签到ID，修改和删除时生效
+    Status: "-1",#状态（0：新增，1：修改，-1：删除）
+    TrainingId: "1"#课程编号
+}
+postForm(url, hash, "创建签到")
